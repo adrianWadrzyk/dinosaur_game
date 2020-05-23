38528cc3dd18b9b3e4a4e4f5d870d1d7c30090ee
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2020-05-23 14:36:49 +0200

    v2.0 add new functions

--- game.js
+++ game.js
@@ -1,174 +1,202 @@
-console.log("dzialam");
-
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-var pong = {};
+
+var time = 0;
+var tmp_table = {};
 
 class Hero {
-    constructor(positionX, positionY, size) {
-            this.positionX = positionX,
-            this.positionY = positionY,
-            this.size = size,
-            this.jumping = false, 
-            this.yVelocity = 0,
-            this.xVelocity =0 
-       }
+  positionX = 20;
+  positionY = 290;
+  size = 10;
+  jumping = false;
+  yVelocity = 0;
 
-       draw () {
-        ctx.beginPath();
-        ctx.moveTo(this.positionX, this.positionY);
-        ctx.lineTo(this.positionX + this.size, this.positionY);
-        ctx.lineTo(this.positionX + this.size, this.positionY + this.size);
-        ctx.lineTo(this.positionX, this.positionY + this.size);
-        ctx.closePath();
-        ctx.stroke();
-        }
-    }
-
-class Board { 
-    constructor (positionY, width) { 
-        this.positionY = positionY, 
-        this.width = width,
-        this.draw = function () {
-            ctx.beginPath();
-            ctx.moveTo(0, positionY);
-            ctx.lineTo(width, positionY);
-            ctx.stroke();
-        } 
-    }
+  draw() {
+    ctx.beginPath();
+    ctx.moveTo(this.positionX, this.positionY);
+    ctx.lineTo(this.positionX + this.size, this.positionY);
+    ctx.lineTo(this.positionX + this.size, this.positionY + this.size);
+    ctx.lineTo(this.positionX, this.positionY + this.size);
+    ctx.closePath();
+    ctx.stroke();
+  }
 }
 
-class Enemy { 
-    constructor (positionX, positionY, height, width) { 
-        this.positionX = positionX,
-        this.positionY = positionY, 
-        this.width = width,
-        this.height = height
-    }
+class Board {
+  positionY = 300;
+  width = canvas.width;
 
-    draw() {
-        ctx.beginPath();
-        ctx.moveTo(this.positionX, this.positionY);
-        ctx.lineTo(this.positionX + this.width, this.positionY);
-        ctx.lineTo(this.positionX + this.width, this.positionY + this.height);
-        ctx.lineTo(this.positionX, this.positionY + this.height);
-        ctx.closePath();
-        ctx.stroke();
-    };
-
-    move(speed) { 
-        this.positionX -=1*speed;
-    }
+  draw() {
+    ctx.beginPath();
+    ctx.moveTo(0, this.positionY);
+    ctx.lineTo(this.width, this.positionY);
+    ctx.stroke();
+  }
 }
 
+class Enemy {
+  
+  constructor(speed) { 
+    this.speed = speed;
+  }  
 
-    controller = { 
-        left : false, 
-        right: false, 
-        up: false,
-        keyListener: function(event) { 
-        let key_state = (event.type == "keydown")?true:false;
-            switch(event.keyCode) { 
-                case 37: 
-                    controller.left = key_state;
-                break;
-                case 38:
-                    controller.up = key_state;
-                break;
-                case 39: 
-                    controller.right = key_state;
-                break;
-            }
-        }
+  positionX = canvas.width;
+  positionY = 270;
+  width = 10;
+  height = 30;
+
+
+  move() {
+    this.positionX -= 1 * this.speed;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.moveTo(this.positionX, this.positionY);
+    ctx.lineTo(this.positionX + this.width, this.positionY);
+    ctx.lineTo(this.positionX + this.width, this.positionY + this.height);
+    ctx.lineTo(this.positionX, this.positionY + this.height);
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+controller = {
+  left: false,
+  right: false,
+  up: false,
+  keyListener: function (event) {
+    let key_state = event.type == "keydown" ? true : false;
+    switch (event.keyCode) {
+      case 37:
+        controller.left = key_state;
+        break;
+      case 38:
+        controller.up = key_state;
+        break;
+      case 39:
+        controller.right = key_state;
+        break;
     }
+  },
+};
 
-    function gameInit () { 
-        pong.board = new Board(300, canvas.width)
-        pong.enemy = [];
-        pong.enemy[0] = new Enemy(200, 270, 30, 10)
-        pong.enemy[1] = new Enemy(400, 270, 30, 10)
-        pong.hero = new Hero(20,290,10);
-    }
-
-    gameInit();
-
-    document.addEventListener("keydown", controller.keyListener);
-    document.addEventListener("keyup", controller.keyListener);
-
-
-function draw() { 
-    pong.board.draw();
-    pong.enemy.forEach(enemy => {
-        enemy.draw();
-        enemy.move(1);
-    });
-    pong.hero.draw();
+function gameInit() {
+  tmp_table.board = new Board();
+  tmp_table.enemy = [];
+  tmp_table.enemy[0] = new Enemy(2);
+  tmp_table.hero = new Hero();
 }
 
-function checkEnemyPosition() { 
-    pong.enemy.forEach(enemy => { 
-        if(enemy.positionX+enemy.width < 0)
-            {
-                enemy.positionX = 250;
-            }
-        })
-}
+gameInit();
 
-function collisions() { 
-    const playerPositionX = Math.floor(pong.hero.positionX+pong.hero.size);
-    const playerPositionY = pong.hero.positionY;
-    
-    pong.enemy.forEach(enemy => {
-        let enemyPostionWidth = enemy.positionX + enemy.width;
-        if (enemy.positionX <= playerPositionX && playerPositionX <= enemyPostionWidth && playerPositionY >= enemy.positionY ) 
-        {
-           console.log("kolizja");
-        } 
-    });
+document.addEventListener("keydown", controller.keyListener);
+document.addEventListener("keyup", controller.keyListener);
 
-
-}
-
-const gameOn = function() { 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if(controller.up && pong.hero.jumping == false)  { 
-        pong.hero.yVelocity -= 20;
-        pong.hero.jumping = true;
-        console.log(pong.hero.positionX)
+function draw() {
+  if(tmp_table.enemy.length == 1)
+   if(Math.random(1) < 0.005){ 
+      tmp_table.enemy.push(new Enemy(tmp_table.enemy[0].speed)); 
     } 
 
-    if(controller.left) { 
-        pong.hero.xVelocity -= 0.5; 
-    }
-
-    
-    if(controller.right) { 
-        pong.hero.xVelocity += 0.5; 
-    }
-    
-
-    pong.hero.yVelocity +=1.5;
-    pong.hero.positionX += pong.hero.xVelocity;
-    pong.hero.positionY += pong.hero.yVelocity;
-    pong.hero.yVelocity *=0.9;
-    pong.hero.xVelocity *=0.9;
-    
-
-    if(pong.hero.positionY > 290) { 
-        pong.hero.jumping = false;
-        pong.hero.positionY = 290;
-        pong.hero.yVelocity = 0;
-    }
-
-    if( pong.hero.positionX <= 0)
-            pong.hero.positionX = 0
-     else if(pong.hero.positionX  >= canvas.width)
-            pong.hero.positionX = canvas.width;
-
-    draw();
-    collisions();
-    checkEnemyPosition();
-    requestAnimationFrame(gameOn);
+  tmp_table.board.draw();
+  tmp_table.enemy.forEach((enemy) => {
+    enemy.draw();
+    enemy.move();
+    console.log(enemy.speed);
+  });
+  tmp_table.hero.draw();
 }
+
+function checkEnemyPosition() {
+   let length = tmp_table.enemy.length;
+   tmp_table.enemy.forEach((enemy) => {
+    if (enemy.positionX + enemy.width < 0) 
+    {
+      if(length > 1){
+          let index = tmp_table.enemy.indexOf(enemy);
+          tmp_table.enemy.splice(index,1);
+        } 
+      enemy.positionX = canvas.width + enemy.width;
+    }
+  });
+}
+
+function rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2) {
+  x2 = Math.round(x2);
+  if (x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2) {
+    return false;
+  }
+  return true;
+}
+
+function collisions(requestID) {
+  const hero = {
+    x: tmp_table.hero.positionX,
+    y: tmp_table.hero.positionY,
+    height: tmp_table.hero.size,
+    width: tmp_table.hero.size,
+  };
+
+  tmp_table.enemy.forEach((enemy) => {
+    {
+      if ( rectIntersect (
+          hero.x,
+          hero.y,
+          hero.width,
+          hero.height,
+          enemy.positionX,
+          enemy.positionY,
+          enemy.width,
+          enemy.height
+        )
+      ) {
+        cancelAnimationFrame(requestID);
+        gameOver();
+      }
+    }
+  });
+}
+
+function gameOver() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "25px Arial black ";
+  ctx.textAlign = "center"
+  ctx.fillText("Przegrałeś!",250,200);  
+}
+
+const gameOn = function () {
+  time++;
+  let point = time / 10;
+
+  if (point % 50 == 0) {
+    tmp_table.enemy.forEach((enemy) => {
+      enemy.speed += 0.1;
+    });
+  }
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (controller.up && tmp_table.hero.jumping == false) {
+    tmp_table.hero.yVelocity -= 25 * 0.5;
+    tmp_table.hero.jumping = true;
+  }
+
+  tmp_table.hero.yVelocity += 0.8;
+  tmp_table.hero.positionY += tmp_table.hero.yVelocity;
+
+  if (tmp_table.hero.positionY > 290) {
+    tmp_table.hero.jumping = false;
+    tmp_table.hero.positionY = 290;
+    tmp_table.hero.yVelocity = 0;
+  }
+
+  ctx.font = "15px Arial black ";
+  ctx.fillText("Liczba punktów: " + Math.floor(point, 1), 30, 50);
+
+  let requestID = requestAnimationFrame(gameOn);
+  draw();
+  collisions(requestID);
+  checkEnemyPosition();
+};
 
 requestAnimationFrame(gameOn);
